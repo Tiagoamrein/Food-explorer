a06a592404281c2579dc39bcc8ccfe48a2d41831
tiago <tiagoamrein@gmail.com>
2023-03-09 22:28:15 -0300

    criando usuario

--- src/Controllers/UsersController.js
+++ src/Controllers/UsersController.js
@@ -1,9 +1,23 @@
 const knex = require("../database")
-
+const AppError = require('../utils/AppError')
 class UsersController {
 
   async create(req, res){
+    const {name, email, password} = req.body
 
-    
+    const userExists = await knex("users").where({ email }).first();
+
+    if(userExists){
+      console.log("email em uso")
+
+    }
+    await knex("users").insert({
+      name,
+      email,
+      password
+    })
+    res.status(201).json()
   }
 }
+
+module.exports = UsersController
--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require ("express")
 const database = require("./database/sqlite")
-
+const routes = require("./routes")
 
 const app = express()
 database()
-
+app.use(express.json()) 
+app.use(routes)
 
 
 const PORT = 3333
